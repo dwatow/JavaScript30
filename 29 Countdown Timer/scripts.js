9d2030679c3f5cacf04e1a40d1a7b9e441ea5f42
chris <dwatow@gmail.com>
2017-04-26 10:30:51 +0800

    add clearInterval

--- 29 Countdown Timer/scripts.js
+++ 29 Countdown Timer/scripts.js
@@ -1,22 +1,28 @@
 const timers = document.querySelectorAll('.timer__button');
 const show_end_time = document.querySelector('.display__end-time');
 const show_remaining_time = document.querySelector('.display__time-left');
+end_time = new Date();
+
+let timer_pid;
 // const custumer_timer = document.querySelector('.customForm');
 
-function countDownTimer(now, end_time) {
+function countDownTimer() {
+	const now = new Date();
 	const remaining_time = new Date(end_time - now);
 	show_remaining_time.textContent = `${remaining_time.getMinutes()}:${remaining_time.getSeconds()}`;
+	if(remaining_time < 0) clearInterval(timer_pid);
 }
 
 function setTimer() {
 	const sec_time = this.dataset.time * 1000;
 	const now = new Date();
-	const end_time = new Date(now.getTime() + sec_time);
+	end_time = new Date(now.getTime() + sec_time);
 
 	const diff = new Date(sec_time);
 	show_end_time.textContent = `倒數到: ${end_time.getHours()}:${end_time.getMinutes()}:${end_time.getSeconds()}`;
 
-	setInterval(() => countDownTimer(now, end_time), 500);
+	clearInterval(timer_pid);
+	timer_pid = setInterval(countDownTimer, 500);
 }
 
 timers.forEach(timer => {
