e065ac667535ece0055a663090c06a2b5f7ed44e
chris <dwatow@gmail.com>
2017-04-26 10:15:50 +0800

    refactory code

--- 29 Countdown Timer/scripts.js
+++ 29 Countdown Timer/scripts.js
@@ -1,10 +1,9 @@
 const timers = document.querySelectorAll('.timer__button');
 const show_end_time = document.querySelector('.display__end-time');
 const show_remaining_time = document.querySelector('.display__time-left');
-let end_time = new Date();
+// const custumer_timer = document.querySelector('.customForm');
 
-function countDownTimer() {
-	const now = new Date();
+function countDownTimer(now, end_time) {
 	const remaining_time = new Date(end_time - now);
 	show_remaining_time.textContent = `${remaining_time.getMinutes()}:${remaining_time.getSeconds()}`;
 }
@@ -12,12 +11,12 @@ function countDownTimer() {
 function setTimer() {
 	const sec_time = this.dataset.time * 1000;
 	const now = new Date();
-	end_time = new Date(now.getTime() + sec_time);
+	const end_time = new Date(now.getTime() + sec_time);
 
 	const diff = new Date(sec_time);
 	show_end_time.textContent = `倒數到: ${end_time.getHours()}:${end_time.getMinutes()}:${end_time.getSeconds()}`;
 
-	setInterval(countDownTimer, 500);
+	setInterval(() => countDownTimer(now, end_time), 500);
 }
 
 timers.forEach(timer => {
